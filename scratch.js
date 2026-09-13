d8e3da3fe0a908c456cf577ac7341e55f9c01092
Eduardo Pacheco <jeduardopacheco0212@gmail.com>
2026-09-13 17:33:26 -0600

    update

--- scratch.js
+++
@@ -1,4 +0,0 @@
-import * as THREE from 'three';
-import fs from 'fs';
-const geom = new THREE.CylinderGeometry(1.5, 1.5, 2, 3);
-console.log("Positions:", geom.attributes.position.count);
--- test_prism.ts
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'three';
-import { getLogicalFaces } from './src/utils/geometry';
-const geom = new THREE.CylinderGeometry(1.5, 1.5, 2, 3);
-let allFaces = getLogicalFaces(geom);
-allFaces = allFaces.filter(f => Math.abs(f.normal.y) < 0.1);
-console.log(allFaces.length);
